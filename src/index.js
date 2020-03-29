f121da69a5f5ecc84e1de8d0424a854eb4e492ff
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2020-03-29 16:49:08 -0300

    nodemailer + /app new testing route

--- src/index.js
+++ src/index.js
@@ -6,19 +6,39 @@ var app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("<h1>vegge.com.br under construction ..</h1>");
+  res.send("<h1>vegge.com.br</h1>");
 });
 
-// app.get("/send", (req, res) => {
-//   nodemailer.createTransport({
-//     host: "smtp.umbler.com",
-//     port: 587,
-//     auth: {
-//       user,
-//       pass
-//     }
-//   });
-// });
+app.get("/app", (req, res) => {
+  res.send({ ok: "true" });
+});
+
+const user = "contato@vegge.com.br";
+const pass = "p@ssMyWORD";
+
+app.get("/send", (req, res) => {
+  const transport = nodemailer.createTransport({
+    host: "smtp.umbler.com",
+    port: 587,
+    auth: {
+      user,
+      pass
+    }
+  });
+
+  transport
+    .sendMail({
+      from: user,
+      to: "cliente@email.com",
+      replyTo: "contato@vegge.com.br",
+      subject: "Novo pedido",
+      text: "1 abacate, 2 bananas, 1 alface crespa."
+    })
+    .then(data => res.send(data))
+    .catch(err => {
+      res.send(err);
+    });
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
