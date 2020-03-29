fe11494956a3d458fa69bfb56732c493ce3bec82
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2020-03-29 16:36:57 -0300

    send under construction

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 var express = require("express");
+var nodemailer = require("nodemailer");
 var cors = require("cors");
 var app = express();
 
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send(<h1>vegge.com.br under construction</h1>);
+  res.send("<h1>vegge.com.br under construction ..</h1>");
 });
+
+// app.get("/send", (req, res) => {
+//   nodemailer.createTransport({
+//     host: "smtp.umbler.com",
+//     port: 587,
+//     auth: {
+//       user,
+//       pass
+//     }
+//   });
+// });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
